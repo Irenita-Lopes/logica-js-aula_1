981b6d91fc3f9d5822c761a6587e33fa342d3fa4
Irenita-Lopes <irenita.f.lopes@gmail.comm>
2025-01-28 17:13:18 -0300

    adicionando gerador de numero pseudo aleatorio

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 alert('Bem-vindo ao jogo do número secreto');
-let numeroSecreto = 4;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
-let tentativas;
+let tentativas=1;
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     if (chute == numeroSecreto) {
-        alert(`Isso aí, você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-        tentativas++;   
+        tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' ;
+    alert(`Isso aí, você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+
